8fa6b14f7520ae37dfbf1ac35f1cde1a8c4f5d72
pgorn <pgornisi@student.wszib.edu.pl>
2023-01-14 14:57:34 +0100

    product

--- orders-api/src/main/java/pl/edu/wszib/orders/api/product/ProductResult.java
+++ orders-api/src/main/java/pl/edu/wszib/orders/api/product/ProductResult.java
@@ -1,4 +1,14 @@
 package pl.edu.wszib.orders.api.product;
 
 public interface ProductResult {
+    ProductApi success();
+    ProductError error();
+
+    default boolean isSuccess() {
+        return success() != null;
+    }
+
+    default boolean isError() {
+        return !isSuccess();
+    }
 }
